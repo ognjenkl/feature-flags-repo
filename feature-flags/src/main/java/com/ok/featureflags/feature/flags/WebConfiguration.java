c33915b5f6c34837fb038f25526dc50baf31509a
ognjenklincov <ognjenkl@gmail.com>
2023-04-25 00:40:01 +0200

    Generated changelog with data, diff empty

--- feature-flags/src/main/java/com/ok/featureflags/feature/flags/WebConfiguration.java
+++ feature-flags/src/main/java/com/ok/featureflags/feature/flags/WebConfiguration.java
@@ -1,22 +1,22 @@
-package com.ok.featureflags.feature.flags;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-/*
-Not sure why this is needed.
-After trying to add spring security to the project and removed,
-after that I was getting 401 unauthorized error.
-When I added this class, it started working.
- */
-@Configuration
-@EnableWebSecurity
-public class WebConfiguration extends WebSecurityConfigurerAdapter {
-
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/**");
-    }
-}
+//package com.ok.featureflags.feature.flags;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+///*
+//Not sure why this is needed.
+//After trying to add spring security to the project and removed,
+//after that I was getting 401 unauthorized error.
+//When I added this class, it started working.
+// */
+//@Configuration
+//@EnableWebSecurity
+//public class WebConfiguration extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    public void configure(WebSecurity web) {
+//        web.ignoring().antMatchers("/**");
+//    }
+//}
